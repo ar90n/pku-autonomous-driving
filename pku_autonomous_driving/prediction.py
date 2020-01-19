4e1e3f51763cc85a9ad036f3b5358dbaab7a1b7f
Masahiro Wada <argon.argon.argon@gmail.com>
2020-01-19 21:54:18 +0900

    backport

--- pku_autonomous_driving/prediction.py
+++ pku_autonomous_driving/prediction.py
@@ -1,10 +1,20 @@
+from pathlib import Path
 import torch
+from torch import nn
 from typing import List
 from tqdm import tqdm_notebook as tqdm
 from .dataset import CarDataset
 from .io import DataRecord
 from .util import extract_coords, coords2str
 import gc
+
+try:
+    from apex import amp
+    has_apex = True
+except ImportError:
+    has_apex = False
+
+use_apex = torch.cuda.is_available() and has_apex
 
 
 def predict(model, loader, device):
@@ -25,3 +35,24 @@ def predict(model, loader, device):
 def clean_up():
     torch.cuda.empty_cache()
     gc.collect()
+
+
+def setup(model: nn.Module, optimizer, device, path: Path = None, opt_level='O1'):
+    checkpoint = {}
+    if path is not None:
+        checkpoint = torch.load(str(path))
+
+    if use_apex:
+        model.to(device)
+        model, optimizer = amp.initialize(model, optimizer, opt_level=opt_level)
+
+    if 'model' in checkpoint:
+        model.load_state_dict(checkpoint['model'])
+    if 'optimizer' in checkpoint:
+        optimizer.load_state_dict(checkpoint['optimizer'])
+    if 'amp' in checkpoint:
+        amp.load_state_dict(checkpoint['amp'])
+
+    if not use_apex:
+        model.to(device)
+    return model, optimizer
